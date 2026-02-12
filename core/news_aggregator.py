2ed770599cee805fa914dd4d9823fa608e5b138c
arunabhamaity148-cell <arunabhamaity148@gmail.com>
2026-02-12 23:26:02 +0530

    Create news_aggregator.py

---
+++ core/news_aggregator.py
@@ -0,0 +1,14 @@
+"""
+News Aggregator
+"""
+
+import logging
+
+logger = logging.getLogger("NEWS")
+
+class NewsAggregator:
+    def __init__(self):
+        pass
+    
+    async def check_news(self):
+        return None
